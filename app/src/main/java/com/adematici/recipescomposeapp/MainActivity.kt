1a19655d1465c453f21c113acfd949e243059a2c
adematici <adematicis41@gmail.com>
2021-08-30 09:17:28 +0300

    Initial commit

---
+++ app/src/main/java/com/adematici/recipescomposeapp/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.adematici.recipescomposeapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.adematici.recipescomposeapp.ui.theme.RecipesComposeAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RecipesComposeAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RecipesComposeAppTheme {
+        Greeting("Android")
+    }
+}
