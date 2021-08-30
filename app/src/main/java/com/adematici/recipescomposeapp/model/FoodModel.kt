1bf84e2d7b6a1353cbb1103738f2b17067c70479
adematici <adematicis41@gmail.com>
2021-08-30 09:38:49 +0300

    FoodModel file added to project for pull data from internet

---
+++ app/src/main/java/com/adematici/recipescomposeapp/model/FoodModel.kt
@@ -0,0 +1,14 @@
+package com.adematici.recipescomposeapp.model
+
+import com.google.gson.annotations.SerializedName
+
+data class FoodModel(
+    @SerializedName("isim")
+    val foodName: String?,
+    @SerializedName("malzeme")
+    val foodIngredient: String?,
+    @SerializedName("yapilis")
+    val foodCook: String?,
+    @SerializedName("gorsel")
+    val foodImage: String?
+)
