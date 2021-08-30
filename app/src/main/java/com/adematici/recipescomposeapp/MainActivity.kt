6e9dedda6cdc6a508dc566863f554cc202545e32
Adem ATİCİ <58858983+aticiadem@users.noreply.github.com>
2021-08-30 10:33:50 +0300

    Project updated, RecipesScreen and FoodDetailScreen added to project (#6)

--- app/src/main/java/com/adematici/recipescomposeapp/MainActivity.kt
+++ app/src/main/java/com/adematici/recipescomposeapp/MainActivity.kt
@@ -3,36 +3,35 @@ package com.adematici.recipescomposeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.adematici.recipescomposeapp.ui.theme.RecipesComposeAppTheme
+import com.adematici.recipescomposeapp.view.FoodDetailScreen
+import com.adematici.recipescomposeapp.view.RecipesScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             RecipesComposeAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                RecipesScreen()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+@Preview(showBackground = true)
+fun RecipesScreenPreview(){
+    RecipesComposeAppTheme {
+        RecipesScreen()
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
+@Preview(showBackground = true)
+fun FoodDetailScreenPreview(){
     RecipesComposeAppTheme {
-        Greeting("Android")
+        FoodDetailScreen()
     }
 }
---
+++ app/src/main/java/com/adematici/recipescomposeapp/view/RecipesScreen.kt
@@ -0,0 +1,9 @@
+package com.adematici.recipescomposeapp.view
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RecipesScreen(){
+    Text(text = "Adem")
+}
