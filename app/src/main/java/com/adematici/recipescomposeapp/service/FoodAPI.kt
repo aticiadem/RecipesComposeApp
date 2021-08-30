67a1054dc94ab1c81d761351012afd42723df8b3
adematici <adematicis41@gmail.com>
2021-08-30 09:53:54 +0300

    Web Service added to project

---
+++ app/src/main/java/com/adematici/recipescomposeapp/service/FoodAPI.kt
@@ -0,0 +1,30 @@
+package com.adematici.recipescomposeapp.service
+
+import com.adematici.recipescomposeapp.model.FoodModel
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+
+class FoodAPI {
+
+    private val api: FoodAPI
+
+    init {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://raw.githubusercontent.com/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        api = retrofit.create(FoodAPI::class.java)
+    }
+
+    suspend fun getRecipes(): List<FoodModel> {
+        return api.getRecipes()
+    }
+
+    interface FoodApi {
+        @GET("aticiadem/YemekKitabiJSONVeriSeti/main/yemekler.json")
+        suspend fun getRecipes(): List<FoodModel>
+    }
+
+}
