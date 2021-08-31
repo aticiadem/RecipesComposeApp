69aaddef5e1384c93fee0d71cf1b49009df2e861
adematici <adematicis41@gmail.com>
2021-08-31 14:44:19 +0300

    Send the selected recipe to the FoodDetail screen

--- app/src/main/java/com/adematici/recipescomposeapp/view/FoodDetailScreen.kt
+++ app/src/main/java/com/adematici/recipescomposeapp/view/FoodDetailScreen.kt
@@ -1,9 +1,31 @@
 package com.adematici.recipescomposeapp.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavController
+import com.adematici.recipescomposeapp.model.FoodModel
+import com.adematici.recipescomposeapp.viewmodel.FoodViewModel
 
 @Composable
-fun FoodDetailScreen(){
-    Text(text = "Adem")
+fun FoodDetailScreen(
+    navController: NavController,
+    name: String
+) {
+    var model: FoodModel
+    val viewModel: FoodViewModel = viewModel()
+    val recipes = viewModel.recipesState.value
+    LazyColumn {
+        items(recipes) { recipe ->
+            if (recipe.foodName == name) {
+                model = recipe
+                Column {
+                    Text(text = model.foodCook)
+                }
+            }
+        }
+    }
 }
